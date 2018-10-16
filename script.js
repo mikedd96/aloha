330798173b47a27586cf010a154dc8b975cfd368
mike <mike_4596@hotmail.com>
2018-10-16 15:33:23 -0700

    finished, removed files

---
+++ script.js
@@ -0,0 +1,58 @@
+// $(document).ready(funtion() {
+
+// smoth scroll from css tricks
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  }); // end of smooth scroll
+  // flickity
+  $('.main-carousel').flickity({
+    // options
+    cellAlign: 'left',
+    contain: true,
+    wrapAround: true,
+    autoPlay: true,
+    prevNextButtons: false,
+  });
+  // flickity end
+
+  // form
+  $('.form').on('submit', function(event) {
+    event.preventDefault();
+    if ($('.submit').val() == '') {
+      alert('Enter a valid email');
+    } else {
+      alert('Thanks for subscribing!');
+    }
+  });
